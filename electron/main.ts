b19a576e5a5d301a38b19e21efbbd2d9180ba22c
JakeGonsalves <gonsalves.jake@gmail.com>
2019-05-09 22:05:56 -0700

    Added sass

--- electron/main.ts
+++ electron/main.ts
@@ -7,26 +7,26 @@ let win: BrowserWindow
 app.on('ready', createWindow)
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow()
+    }
 })
 
 function createWindow() {
-  // win = new BrowserWindow({ fullscreen: true })
-  win = new BrowserWindow({ width: 800, height: 600 })
+    // win = new BrowserWindow({ fullscreen: true })
+    win = new BrowserWindow({ width: 800, height: 600 })
 
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/../../dist/angular-electron/index.html`),
-      protocol: 'file:',
-      slashes: true,
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/../../dist/angular-electron/index.html`),
+            protocol: 'file:',
+            slashes: true,
+        })
+    )
+
+    // win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
     })
-  )
-
-  // win.webContents.openDevTools()
-
-  win.on('closed', () => {
-    win = null
-  })
 }
